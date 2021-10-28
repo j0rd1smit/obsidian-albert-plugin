da79fc715cb1159c9a76bfe02b9ffd38520e3a94
Jordi <jordismit20000@gmail.com>
2021-10-28 21:21:14 +0200

    init

--- __init__.py
+++ __init__.py
@@ -14,8 +14,7 @@ import os
 from time import sleep
 from pathlib import Path
 import sys
-import Levenshtein as lev
-
+import json
 
 __title__ = "Obsidian"
 __version__ = "0.0.0"
@@ -24,15 +23,38 @@ __authors__ = "J0rd1smit"
 __exec_deps__ = []
 __py_deps__ = []
 
+PATH_TO_CONFIG_FOLDER = Path.home() / ".config/albert/obsidian-plugin"
+PATH_TO_CONFIG_DATA = PATH_TO_CONFIG_FOLDER / "config.json"
+PATH_TO_ICON = os.path.dirname(__file__) + "/plugin.png"
+
 iconPath = iconLookup("albert")
 
-vault_path = Path("/home/jordi/Dropbox/obsidian")
-vault_name = "obsidian"
+#vault_path = Path("/home/jordi/Dropbox/obsidian")
+#vault_name = "obsidian"
 
 
-# Can be omitted
 def initialize():
-    pass
+    PATH_TO_CONFIG_FOLDER.mkdir(parents=True, exist_ok=True)
+
+    if not PATH_TO_CONFIG_DATA.exists():
+        #PATH_TO_CONFIG_DATA.touch()
+        _create_default_config()
+
+            
+def _create_default_config():
+    with open(PATH_TO_CONFIG_DATA, "w") as f:
+        data = {
+            "vault_name": "obsidian",
+            "path_to_vault": str(Path.home() / "obsidian"),
+            "commands": [
+                {
+                    "name": "New note",
+                    "subtext": "Add a new note to the vault",
+                    "uri": "obsidian://new?vault={{vault}}&name={{q}}"
+                }
+            ],
+        }
+        json.dump(data, f, indent=4)
 
 
 # Can be omitted
@@ -45,51 +67,95 @@ def handleQuery(query):
         return
     
     query_text = query.string.lower()
-    items = []
-    max_n_matches = 0
 
-    for path in vault_path.rglob("*.md"):
-        n_matches = _n_matches(path, query_text)
+    with open(PATH_TO_CONFIG_DATA) as f:
+        config = json.load(f)
 
-        path_rel_to_value = str(path).replace(str(vault_path), "")
-        action = UrlAction(
-            text="UrlAction",
-            url=f"obsidian://open?vault={vault_name}&file={path_rel_to_value.replace(' ', '%20')}",
-        )
-        item = Item(id=__title__,
-            icon=os.path.dirname(__file__)+"/plugin.png",
-            text=path.name,
-            subtext=path_rel_to_value,
-            actions=[action],
-        )
-        items.append((item, n_matches))
-        max_n_matches = max(max_n_matches, n_matches)
+    vault_name = config["vault_name"]
+    path_to_vault = Path(config["path_to_vault"])
+    commands = config["commands"]
+
+    item_ranking_pairs = []
+    max_ranking = 0
+
+    for path in path_to_vault.rglob("*.md"):
+        item, ranking = create_open_note_item(vault_name, path_to_vault, path, query_text)
+
+        max_ranking = max(max_ranking, ranking)
+        item_ranking_pairs.append((item, ranking))
 
 
+    for command in commands:
+        item, ranking = create_uri_item(query_text, command)
+        max_ranking = max(max_ranking, ranking)
+        item_ranking_pairs.append((item, ranking))
 
-    n_matches = _n_matches("quick add", query_text)
+    return [item for (item, ranking) in item_ranking_pairs if ranking == max_ranking]
+
+def create_open_note_item(
+        vault_name,
+        path_to_vault, 
+        path_to_file,
+        query,
+    ):
+    path_rel_to_vault = str(path_to_file).replace(str(path_to_vault), "")
+    path_rel_to_vault_encoded = to_uri_encoding(path_rel_to_vault)
+
     action = UrlAction(
         text="UrlAction",
-        url=f"obsidian://advanced-uri?commandname=QuickAdd%3A%20Run%20QuickAdd",
+        url=f"obsidian://open?vault={vault_name}&file={path_rel_to_vault_encoded}",
     )
-    item = Item(id=__title__,
-        icon=os.path.dirname(__file__)+"/plugin.png",
-        text="Quick Add",
-        subtext="",
+    item = Item(
+        id=__title__,
+        icon=PATH_TO_ICON,
+        text=path_to_file.name,
+        subtext=path_rel_to_vault,
         actions=[action],
     )
-    items.append((item, n_matches))
-    max_n_matches = max(max_n_matches, n_matches)
 
-           
+    return item, _n_matches(path_rel_to_vault, query)
+
+def to_uri_encoding(s):
+    return s.replace(' ', '%20').replace("/", "%2F")
 
 
-    return [i[0] for i in items if i[1] == max_n_matches]
-
-def _n_matches(path, query):
+def _n_matches(key, query):
+    key = key.lower()
     matches = 0
     for search_key in query.split(" "):
-        if search_key in str(path).lower():
+        if search_key in key:
             matches += 1
 
     return matches
+
+
+def create_uri_item(
+        query,
+        uri_config,
+    ):
+    
+    name = uri_config["name"]
+    subtext = uri_config["subtext"]
+    uri_template = uri_config["uri_template"]
+
+    q = query.lower()
+    for i in name.lower().split():
+        q = q.replace(i, "")
+
+
+    action = UrlAction(
+        text="UrlAction",
+        url=uri_template.replace("{{q}}", to_uri_encoding(q)),
+    )
+    item = Item(id=__title__,
+        icon=PATH_TO_ICON,
+        text=name,
+        subtext=subtext,
+        actions=[action],
+    )
+    key = name
+    if len(subtext) > 0:
+        key = " " + subtext
+
+    return item, _n_matches(key, query)
+
